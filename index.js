29c3811557c66dda3ef220b7ba16dac66b6b4b18
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-10-01 21:33:24 +0600

    api add

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use('/qurinom-solutions/api/v1', require('./router/index.js'))
 
-
+app.get('/get',async(req,res)=>{
+    res.status(200).json({
+        message:'Success',
+        status:true
+    })
+})
 
 //connectivity
 const Start = async () => {
